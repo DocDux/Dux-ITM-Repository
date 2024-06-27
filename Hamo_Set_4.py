f0b85fd03f365ed5eee41ba6d66853e7264ce342
DocDux <rosendo.hamo@student.ateneo.edu>
2024-06-28 06:55:02 +0800

    Formative Set 4

---
+++ Hamo_Set_4.py
@@ -0,0 +1,216 @@
+'''
+Programming Set 4
+
+This assignment will test your proficiency in pattern recognition
+and programming in Python.
+'''
+
+def bin_to_dec(binary_string):
+    '''
+    Convert a binary string to its base-10 integer representation.
+    
+    Example:
+    bin_to_dec("101") -> 5
+    bin_to_dec("1010") -> 10
+    bin_to_dec("11111111") -> 255
+    bin_to_dec("0") -> 0
+    bin_to_dec("1") -> 1
+    
+    Parameters
+    ----------
+    binary_str: str
+        The binary string to be converted to a base-10 integer.
+        The string should contain only the characters '0' and '1'.
+    
+    Returns
+    -------
+    int
+        The base-10 integer representation of the input binary string.
+    
+    Notes
+    -----
+    Binary is a base-2 number system that uses only two digits: 0 and 1. 
+    Each digit in a binary number is called a "bit". The position of each bit 
+    represents a power of 2, starting from the rightmost bit (2^0), the next bit 
+    to the left (2^1), and so on. To understand a binary number, convert it to 
+    decimal by summing the products of each bit and its corresponding power of 2.
+    
+    For example, the binary string "1011" is calculated as:
+    (1 * 2^3) + (0 * 2^2) + (1 * 2^1) + (1 * 2^0)
+    = 8 + 0 + 2 + 1 = 11
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.    
+
+    # accumulator variable
+    binary_sum = 0
+    counter = 0
+
+    reversed_binary_string = binary_string[::-1]
+    for number in reversed_binary_string:
+        binary_sum += int(number) * (2**counter)
+        counter += 1
+
+    return binary_sum
+
+bin_to_dec("1010")
+
+def dec_to_bin(number):
+    '''
+    Convert a base-10 number to its binary string representation.
+    
+    Example:
+    dec_to_bin(5) -> "101"
+    dec_to_bin(10) -> "1010"
+    dec_to_bin(255) -> "11111111"
+    dec_to_bin(0) -> "0"
+    dec_to_bin(1) -> "1"
+    
+    Binary is a base-2 number system that uses only two digits: 0 and 1. 
+    Each digit in a binary number is called a "bit". The position of each bit 
+    represents a power of 2, starting from the rightmost bit (2^0), the next bit 
+    to the left (2^1), and so on. To understand a binary number, convert it to 
+    decimal by summing the products of each bit and its corresponding power of 2.
+    
+    Parameters
+    ----------
+    number: int
+        The base-10 integer to be converted to a binary string.
+        The number should be a non-negative integer.
+    
+    Returns
+    -------
+    str
+        The binary string representation of the input base-10 number.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+
+   # how to perform the conversion from decimal to binary (mathematically)
+# 1. Divide the number by 2
+# 2. Get the integer quotient (which we will divide again by 2)
+# 3. Get the remainder (this will be our binary digit)
+# 4. Repeat the steps above until the quotient is equal to 0
+# 5. Our final binary string will be the "list" of remainders in #3 joined together (IN REVERSE ORDER)
+
+    binary_string_accumulator = ""
+    remaining_number = number
+
+    if remaining_number == 0:
+        binary_string_accumulator += "0"
+    else: 
+        
+        while remaining_number // 2 > 0:
+            binary_digit = remaining_number % 2
+            binary_string_accumulator += str(binary_digit)
+            remaining_number = remaining_number // 2
+
+        if remaining_number // 2 == 0:
+            if remaining_number < 2:
+                if remaining_number > 0:
+                    binary_string_accumulator += "1"
+        
+    final_binary_string = binary_string_accumulator[::-1]
+
+    return final_binary_string
+
+dec_to_bin(0)
+
+#3 - telephone cipher
+encoder_dict = {
+        " ":"0",
+        "A":"2",
+        "B":"22",
+        "C":"222",
+        "D":"3",
+        "E":"33",
+        "F":"333",
+        "G":"4",
+        "H":"44",
+        "I":"444",
+        "J":"5",
+        "K":"55",
+        "L":"555",
+        "M":"6",
+        "N":"66",
+        "O":"666",
+        "P":"7",
+        "Q":"77",
+        "R":"777",
+        "S":"7777",
+        "T":"8",
+        "U":"88",
+        "V":"888",
+        "W":"9",
+        "X":"99",
+        "Y":"999",
+        "Z":"9999"
+    }
+
+def telephone_cipher(message):
+    '''
+    Before, when phones did not have touchscreen keypads, 
+    the way to input letters was to click the physical keypads 
+    repeatedly.
+    
+    For example:
+    Clicking "222" will result in the letter C. 
+    Clicking "7777" will result in the letter S. 
+    and so on
+    
+    To read more about it, you may visit the following link:
+    Telephone Keypad: https://en.wikipedia.org/wiki/Keypad
+    
+    Using the `encoder_dict` in "set_4_given.py",
+    your task is to convert a letter string into its equivalent
+    numerical string as typed in a Telephone Keypad.
+    
+    Note: In the case of text inputs like "ABC", to demarcate the
+    same letters, an underscore "_" is placed in between.
+    
+    Examples:
+    telephone_cipher("ABC") -> "2_22_222"
+    telephone_cipher("HELLO WORLD") -> "4433555_555666096667775553"
+    telephone_cipher("TEST") -> "83377778"
+    telephone_cipher("HOW DO YOU DO") -> "4466690366609996668803666"
+    telephone_cipher("ABRACADABRA") -> "2_227772_222_232_227772"
+    
+    Parameters
+    ----------
+    message: str
+        the text string consisting of capital letters
+    
+    Returns
+    -------
+    str
+        the equivalent numerical string typed in a Telephone Keypad
+        with underscores demarcating characters who share the same key
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+
+    #accumulator variable
+    tele_cipher = ""
+    counter = 0
+
+    # cipher
+    for letter in message:
+        encoder_number = encoder_dict[letter]
+        tele_cipher += encoder_number
+
+        # demarcating
+        # checks if we are not at the last character of the message. if we don't have this, program can go out of bounds
+        if counter < len(message) - 1:
+            # gets the encoding "value" of the next letter
+            encoder_next_number = encoder_dict[message[(counter+1)]]
+            # if the current letter and the next letter use the same key, they will have the same starting numbers
+            if encoder_number[0] == encoder_next_number[0]:
+                tele_cipher += "_"
+
+        counter += 1
+
+    return tele_cipher
+
+telephone_cipher("ABRACADABRA")
+
+#
